efd6dc09845b1848a5c7dd55e9e52ff4c60fae3f
DoggoDev-0 <bogdanandreiana13@gmail.com>
2023-02-21 21:50:30 +0200

    Install mongoose

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/healthlink', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB...'))
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
