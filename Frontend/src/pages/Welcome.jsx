34781cd58f496fa92ef917d8f9fb959180c52b3c
DoggoDev-0 <bogdanandreiana13@gmail.com>
2023-03-21 21:08:17 +0200

    Add desktop welcome page

--- Frontend/src/pages/Welcome.jsx
+++ Frontend/src/pages/Welcome.jsx
@@ -1,38 +1,63 @@
 import { Link } from "react-router-dom";
 import styles from "./Welcome.module.scss";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 export default function Welcome() {
+	const desktop = useMediaQuery("(min-width:768px)");
+
 	return (
 		<div className={styles.container}>
-			<div className={styles.logoContainer}>
-				<img className={styles.logoImg} src="logo.svg" alt="" />
-				<p className={styles.logoText}>Healthlink</p>
-			</div>
-			<div className={styles.header}>
-				<img className={styles.headerPhoto} src="welcome_header.svg" alt="" />
-			</div>
-			<div className={styles.title}>
-				<h1>Welcome!</h1>
-				<h2>Connecting you to better healthcare, one click at a time.</h2>
-			</div>
-			<div className={styles.fullWidth}>
-				<div className={styles.buttonContainer}>
-					<Link to="signup">
-						<button className={`${styles.button} ${styles.signup}`}>
-							Sign up
-						</button>
-					</Link>
-					<Link to="login">
-						<button className={`${styles.button} ${styles.login}`}>
-							Sign in
-						</button>
-					</Link>
+			<div className={styles.containerLg}>
+				{!desktop && (
+					<div className={styles.logoContainer}>
+						<img className={styles.logoImg} src="logo.svg" alt="" />
+						<p className={styles.logoText}>Healthlink</p>
+					</div>
+				)}
+				<div className={styles.header}>
+					{!desktop && (
+						<img
+							className={styles.headerPhoto}
+							src="welcome_header.svg"
+							alt=""
+						/>
+					)}
+					{desktop && (
+						<img
+							className={styles.headerPhoto}
+							src="welcome_header_lg.svg"
+							alt=""
+						/>
+					)}
 				</div>
+				<div className={styles.title}>
+					<h1>Welcome!</h1>
+					<h2>Connecting you to better healthcare, one click at a time.</h2>
+				</div>
+				<div className={styles.buttonPositioning}>
+					<div className={styles.buttonContainer}>
+						<Link to="signup">
+							<button className={`${styles.button} ${styles.signup}`}>
+								Sign up
+							</button>
+						</Link>
+						<Link to="login">
+							<button className={`${styles.button} ${styles.login}`}>
+								Sign in
+							</button>
+						</Link>
+					</div>
+				</div>
+				{!desktop && (
+					<div className={styles.triangleContainer}>
+						<div className={styles.triangle}></div>
+						<div className={styles.triangle}></div>
+					</div>
+				)}
 			</div>
-			<div className={styles.triangleContainer}>
-				<div className={styles.triangle}></div>
-				<div className={styles.triangle}></div>
-			</div>
+			{desktop && (
+				<img src="welcome_leaves_lg.svg" alt="" className={styles.leaves} />
+			)}
 		</div>
 	);
 }
